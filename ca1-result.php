836b94f18c28872e67eec5a3a49a4ccafdcc747b
Elizabeth Doran Burke <lizziede@gmail.com>
2023-11-04 20:57:26 +0000

    Initial commit

---
+++ ca1-result.php
@@ -0,0 +1,53 @@
+<!--User has choosen 'RADIO BUTTON' of selected artists and clicked Submit, therefore user will now see artists name, image, bio and soundclip for one of three of each genre-->
+<!DOCTYPE html>
+<?php
+$selectedArtist=$_POST["selectedArtist"];
+?>
+<html>
+
+<head>
+   <link rel="stylesheet" type="text/css" href="style1.css">
+    <title>Artist Page</title>
+</head>
+
+<body>
+    <div class=artistName>
+        <?php
+        $nameArtist=basename($selectedArtist,'.png');
+        echo '<h2>'.ucwords(str_replace('-',' ',$nameArtist)).'</h2>';
+        ?>
+    </div>
+    <div class=artistImage>
+        <?php
+        echo '<img src="'.$selectedArtist.'">';
+        ?>
+    </div>
+    <div class=artistSound>
+        <?php
+        $soundDir=dirname("$selectedArtist");
+        $soundBase=basename("$nameArtist.mp3");
+        $soundFull=("$soundDir/$soundBase");
+        if (file_exists($soundFull)){
+        echo '<audio controls>';
+        echo '<source src="'.$soundFull.'" type="audio/mpeg">';
+        echo '</audio>';
+        } else {
+            echo "Sorry, no Sound Clip found for ".ucwords(str_replace('-',' ',$nameArtist))."";
+        }
+        ?>
+    </div>
+    <div class=artistBio>
+        <?php
+        $bioTextDir=dirname("$selectedArtist");
+        $bioTextBase=basename("$nameArtist.txt");
+        $bioTextFull=("$bioTextDir/$bioTextBase");
+        if (file_exists($bioTextFull)){
+        echo file_get_contents($bioTextFull);
+        } else {
+            echo "Sorry, no Bio Information found for ".ucwords(str_replace('-',' ',$nameArtist))."";
+        }
+        ?>
+    </div>
+    <button name="home" type="submit" value="$allGenres">Home</button>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<?php
+        $filter=$_POST['allGenres'];
+        ?>
+<html lang="en-uk">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="author" content="Liz Doran Burke">
+    <meta name="discription" content="This is part of a Media Systems web space where users can view a list of music artists, their Genres and some information about the artist.">
+    <link rel="stylesheet" type="text/css" href="style1.css">
+    <title>Beats Central</title>
+</head>
+
+<body>
+
+    <div class="titleHeader">
+        <h1>Beats Central</h1>
+    </div>
+    <div class="titleText">
+        <p>Here you can filter artists by Genre</p>
+    </div>
+    <!--First part of 'form'-->
+    <form method="POST" action="index.php">
+        <!--Genre Drop Down Menu Section-->
+        <div class="dropdown">
+            <label for="allGenres">Select a Genre:</label><br>
+            <select name="allGenres" id="allGenres">
+                <option value="">All Music</option>
+                <?php
+            $allGenres=glob('*',GLOB_ONLYDIR);    
+            for ($i=0;$i<count($allGenres);$i++){
+            echo '<option ';
+            echo 'value="'.$allGenres[$i].'"';
+                if($allGenres[$i]==$filter){
+                echo ' selected';
+            }
+            echo '>'.$allGenres[$i].'</option>';
+            echo PHP_EOL;
+            }
+            ?>
+               <!--Filter button for drop down menu-->
+                <input type="submit" id="allGenres" value="Filter">
+            </select>
+        </div>
+    </form>
+    <!--Second part of 'form'-->
+    <form method="POST" action="index.php">
+        <div class="allGenres">
+            <?php
+            /*$allGenres is the box with ALL Genre folders Blues, Rock etc inside it*/
+            $allGenres=glob('*',GLOB_ONLYDIR);
+            /*This section is the title of the Genre folders*/
+            for($i=0;$i<count($allGenres);$i++){
+                echo '<div class="selectedGenre">';
+                if($allGenres[$i]==$filter||$filter==''){
+                echo '<h2>'.$allGenres[$i].'</h2>';
+                            
+            /*$selectedGenre is the box with ONE of the Genre folders inside it ($allGenres but $i selects ONE of them)*/
+                $selectedGenre=glob($allGenres[$i].'/*.png');
+                
+                for($j=0;$j<count($selectedGenre);$j++){
+                    echo '<div class="selectedArtist">';
+                    /*This section is the artists name and image within the Genre folders*/        
+                    $strippedArtist= basename($selectedGenre[$j],'.png');
+                    echo '<h3>'.ucwords(str_replace('-',' ',$strippedArtist)).'</h3>';
+                    echo '<img src="'.$selectedGenre[$j].'" width="200px" height="auto">';
+                    echo '<label for="selectedArtist"></label>';
+                    echo '<input type="radio" value="'.$selectedGenre[$j].'" name="selectedArtist">';
+                    echo '<button type="submit" value="Submit" formaction="ca1-result.php">More Info</button>';
+                    echo '</div>';
+                }
+                }
+               echo '</div>'; 
+            echo PHP_EOL;
+                        }   
+            ?>
+
+        </div>
+    </form>
+</body>
+
+</html>
